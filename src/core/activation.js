22a699c29524e360e3e287cb8bb5bbbd690fb164
dhan <kyrawillowxd@gmail.com>
2026-08-17 15:07:13 +0700

    Membuat perceptron Learning dan juga memprediksi data mentah

--- src/core/activation.js
+++ src/core/activation.js
@@ -1,9 +1,13 @@
 const threshold = 0;
 
 export function stepFunction(x) {
-  if (x >= threshold) {
-    return 1;
-  } else {
-    return 0;
+  let result = [];
+  for (let i = 0; i < x.length; i++) {
+    if (x[i] >= threshold) {
+      result.push(1);
+    } else {
+      result.push(0);
+    }
   }
+  return result;
 }
--- src/core/perceptron.js
+++ src/core/perceptron.js
@@ -1,14 +1,16 @@
-export function perceptron(x) {
+import { stepFunction } from "./activation.js";
+
+export function dotProduct(x) {
   // Input masuk (nIn)
   let nIn = x.length;
-  console.info(nIn);
+  //   console.info(nIn);
 
   // Next layer 3 Neuron (nilai tersebut berdasarkan keputusanku)
   let nOut = 3;
 
   // Xaviera/Glorot Uniform
   let xaviera = 6 / (nIn + nOut);
-  console.info(xaviera);
+  //   console.info(xaviera);
 
   let w = [];
 
@@ -24,11 +26,37 @@ export function perceptron(x) {
     w.push(neuronInput);
   }
 
-  console.info(w);
+  return w;
 
-  let min = -2;
-  let max = 2;
-  console.info(Math.random() * (max - min) + min);
+  //   let min = -2;
+  //   let max = 2;
+  //   console.info(Math.random() * (max - min) + min);
 }
 
-perceptron([1, 2, 3]);
+export function perceptronLearning(x, weight) {
+  // Untuk BIAS awal kita definisikan manual terlebih dahulu
+  let b = 0;
+
+  let z = [];
+
+  //   console.info(x[0]);
+  //   console.info(weight[0][0]);
+  //   console.info(weight.length);
+
+  for (let i = 0; i < x.length; i++) {
+    let result = 0;
+    for (let j = 0; j < weight.length; j++) {
+      let multiplication = x[j] * weight[i][j];
+      result += multiplication;
+    }
+    z.push(result);
+  }
+
+  let step = stepFunction(z);
+  return step;
+}
+
+let data = [1, 2, 3];
+let weight = dotProduct(data);
+let perceptron = perceptronLearning(data, weight);
+console.info("ini perception " + perceptron);
